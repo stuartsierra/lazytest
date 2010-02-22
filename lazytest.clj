908875fc97c0b08c4a5e929f38d53c40650f0229
Stuart Sierra <mail@stuartsierra.com>
2010-02-22 10:12:26 -0500

    Return to deftypes

--- lazytest.clj
+++ lazytest.clj
@@ -1,16 +1,11 @@
-;; I want to use deftype, but literal deftype'd objects cannot be
-;; 9embedded in generated code.
+(deftype Context [before after])
 
-(defn TestCase [contexts locals children]
-  (with-meta {:contexts contexts
-              :locals locals
-              :children children}
-    {:type ::TestCase}))
+(deftype Assertion [locals form])
 
-(defn Context [before after]
-  (with-meta {:before before
-              :after after}
-    {:type ::Context}))
+(deftype TestCase [contexts children])
+
+(defprotocol TestCompilable
+  (test-compile [x]))
 
 (defprotocol TestSuccess
   (success? [r] "Returns true if r is a 100% successful result."))
@@ -28,28 +23,15 @@
   TestSuccess (success? [] false))
 
 (defn format-assertion [a]
-  `(try (if ~a
-          (AssertionPassed '~a)
-          (AssertionFailed '~a))
-        (catch Throwable t#
-          (AssertionThrown '~a t#))))
-
-(defn format-tests [t]
-  (let [active (gensym "active")]
-    `(fn [~active ~@(:locals t) & states#]
-       (lazy-seq
-        ~(loop [r nil
-                children (reverse (:children t))]
-           (if (seq children)
-             (let [c (first children)]
-               (recur (if (= ::TestCase (type c))
-                        `(cons (run-tests '~c ~active) ~r)
-                        `(cons ~(format-assertion c) ~r))
-                      (next children)))
-             r))))))
-
-(defn compile-tests [t]
-  (eval (format-tests t)))
+  {:pre [(= ::Assertion (type a))
+         (vector? (:locals a))]}
+  (let [form (:form a)]
+    `(fn ~(vec (cons (gensym) (:locals a)))
+       (try (if ~form
+              (AssertionPassed '~form)
+              (AssertionFailed '~form))
+            (catch Throwable t#
+              (AssertionThrown '~form t#))))))
 
 (defn unchunked-map [f coll]
   (lazy-seq
@@ -57,35 +39,44 @@
      (cons (f (first coll))
            (unchunked-map f (next coll))))))
 
-(defn run-tests [t active]
-  (let [contexts (:contexts t)
-        states (map (fn [c] (or (active c) ((:before c)))) contexts)
-        ;;_ (prn "States are " states)
-        merged (merge active (zipmap contexts states))
-        ;;_ (prn "Merged is " merged)
-        results (apply (compile-tests t) merged states)
-        closed (map (fn [c state]
-                      (when-not (contains? active c) ((:after c))))
-                    (reverse contexts) (reverse states))]
-    (TestResult t results)))
+(def mtest-compile (memoize test-compile))
 
-(defn doall-results [tr]
-  (doall (tree-seq :children :children tr)))
+(extend-protocol TestCompilable
+ ::Assertion
+ (test-compile [a] (eval (format-assertion a)))
+ ::Context
+ (test-compile [c] (fn ([] (when-let [f (:before c)] (f)))
+                     ([state] (when-let [f (:after c)] (f state)))))
+ ::TestCase
+ (test-compile [t]
+  (let [cfns (map mtest-compile (:contexts t))
+        tfns (map mtest-compile (:children t))
+        open (fn [active] (map (fn [f]
+                                 (prn "Trying to open cfn" f)
+                                 (or (active f) (f))) cfns))
+        close (fn [active states]
+                  (map (fn [c state]
+                         (when-not (contains? active c) (c state)))
+                       (reverse cfns) (reverse states)))]
+    (fn [active & states]
+      (prn "Active is" active)
+       (let [states (open active)
+             merged (merge active (zipmap cfns states))
+             _ (prn "Merged is" merged)
+             results (unchunked-map #(apply % merged states) tfns)]
+         (close active states)
+         (TestResult (str t) results))))))
+
+(defn results-seq [r]
+  (tree-seq :children :children r))
 
 (def c1 (Context (fn [] (prn "Opening lazy context c1.") 1) nil))
 
-(def c2 (Context (fn [] (prn "Opening lazy context c2.") 2.5) nil))
+(def a1 (Assertion '[a] '(do (prn "Executing assertion a1.")
+                             (pos? a))))
 
-(def t1 (TestCase [c1 c2] '[a b]
-                  '[(do (prn "Evaluating test t1-a") (integer? a))
-                    (do (prn "Evaluating test t1-b") (integer? b))]))
+(def t1 (TestCase [c1] [a1]))
 
-(def t2 (TestCase [c1 c2] '[a b]
-                  '[(do (prn "Evaluating test t2-a") (integer? a))
-                    (do (prn "Evaluating test t2-b") (integer? b))]))
+(def t2 (TestCase [c1] [a1 a1 a1]))
 
-(def s1 (TestCase [] [] [t1 t2]))
-
-(def s2 (TestCase [c1 c2] [] [t1 t2]))
-
-(def s3 (TestCase [] [] [t1 t2 t1 t2 t1 t2]))
+(def t3 (TestCase [c1] [t1 t1 t1]))
