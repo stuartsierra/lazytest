245da9007098b5aa1b1bceb636075f8f5c375267
Stuart Sierra <mail@stuartsierra.com>
2010-02-22 10:11:24 -0500

    Second draft: lazy test result generation

--- lazytest.clj
+++ lazytest.clj
@@ -1,7 +1,7 @@
 ;;; TEST2: a new testing framework for Clojure
 
 ;; by Stuart Sierra, http://stuartsierra.com/
-;; February 18, 2010
+;; February 19, 2010
 
 ;; Copyright (C) 2010 Stuart Sierra.
 
@@ -13,21 +13,18 @@
 ;; from this software.
 
 
+;; Second draft: Lazy results
+
+
 (ns clojure.test2)
-
-(def *active-contexts* {})
-
-(def *defining-context-bindings* [])
 
 (defprotocol Successful
   (success? [r] "Returns true if the TestResult or 
   SuiteResult was 100% successful."))
 
-(defprotocol TestCompilable
-  (compile [x active-contexts] "Compiles the Test or Suite to a fn."))
-
 (defprotocol TestRunnable
-  (run [x] "Executes the Test or Suite."))
+  (run-tests [x actives]
+   "Executes the Test or Suite with a map of active contexts."))
 
 (deftype Test [locals contexts assertions])
 
@@ -44,8 +41,8 @@
 (deftype AssertionThrown [form throwable]
   Successful (success? [] false))
 
-(deftype TestResult [test assertion-results]
-  Successful (success? [] (every? success? assertion-results)))
+(deftype TestResult [test child-results]
+  Successful (success? [] (every? success? child-results)))
 
 (deftype SuiteResult [suite child-results]
   Successful (success? [] (every? success? child-results)))
@@ -102,74 +99,90 @@
 
 (defn- open-context 
   "Calls Context c's :before function, returns result."
-  [active-contexts c]
-  (or (active-contexts c)
+  [actives c]
+  (or (actives c)
       (when-let [f (:before c)]
         (f))))
 
 (defn- close-context
   "Calls Context c's :after function on state."
-  [active-contexts c state]
-  (when-not (contains? active-contexts c)
+  [actives c state]
+  (when-not (contains? actives c)
     (when-let [f (:after c)]
       (f state))))
 
-(defn- call-with-context
-  "Opens all contexts, applies f to their state, closes all contexts
-  in reverse order, and returns f's return value.  Executes strictly
-  if any contexts has an :after function; otherwise executes lazily."
-  [active-contexts f contexts]
-  {:pre [(every? context? contexts) (fn? f)]}
-  (if (some :after contexts)
-    ;; strict (non-lazy)
-    (let [states (doall (map (partial open-context active-contexts)
-                             contexts))
-          result (doall (apply f states))]
-      (doall (map (partial close-context active-contexts)
-                  (reverse contexts) (reverse states)))
-      result)
-    ;; lazy
-    (apply f (map open-context contexts))))
+(defn- unchunked-map
+  "Purely lazy map that processes chunked sequences one at a time."
+  [f coll]
+  (lazy-seq
+   (when (seq coll)
+     (cons (f (first coll))
+           (unchunked-map f (next coll))))))
 
-(defn- compile-suite [active-contexts s]
-  (or (::compiled (meta s))
-      (fn [& _] (map (partial active-contexts run)
-                     (:children s)))))
-
-(extend-protocol TestCompilable
+(extend-protocol TestRunnable
   ::Test
-  (run [active-contexts t]
-       (TestResult t (call-with-context
-                        active-contexts
-                        (compile-test s) (:contexts t))))
+  (run-tests [t actives]
+    (let [f (compile-test t)]
+      (if (some :after (:contexts t))
+        ;; strict (non-lazy)
+        (let [states (doall (map (partial open-context actives) (:contexts t)))
+              result (doall (apply f states))]
+          (doall (map (partial close-context actives)
+                      (reverse (:contexts t)) (reverse states)))
+          (TestResult t result))
+        ;; lazy
+        (let [states (map (partial open-context actives) (:contexts t))]
+          (TestResult t (apply f states))))))
   ::Suite
-  (run [active-contexts s]
-       (SuiteResult s (call-with-context
-                        active-contexts
-                        (compile-suite s) (:contexts s)))))
+  (run-tests [s actives]
+    (if (some :after (:contexts s))
+      ;; strict (non-lazy)
+      (let [states (doall (map (partial open-context actives) (:contexts s)))
+            new-actives (merge actives (zipmap (:contexts s) states))
+            result (doall (map #(run-tests % new-actives) (:children s)))]
+        (doall (map (partial close-context actives)
+                    (reverse (:contexts s)) (reverse states)))
+        (SuiteResult s result))
+      ;; lazy
+      (let [states (map (partial open-context actives) (:contexts s))
+            new-actives (merge actives (zipmap (:contexts s) states))]
+        (SuiteResult s (unchunked-map #(run-tests % new-actives) (:children s)))))))
 
-(defn run-with-active-context [active-contexts x]
-  )
 
+(defn run "Executes the Test or Suite."
+  [x] (run-tests x {}))
+
+(derive ::TestResult ::CollectedResult)
+(derive ::SuiteResult ::CollectedResult)
+
+(defn doall-results
+  "Forces all lazy test results to be evaluated."
+  [x]
+  (cond (isa? (type x) ::CollectedResult)
+          (dorun (map doall-results (:child-results x)))
+        (seq? x)
+          (dorun x)
+        :else x)
+  x)
 
 ;;; RAW API USAGE:
 
 (def c1 (Context (fn [] (prn "Opening context c1.") 1)
-                 (fn [_] (prn "Closing context c1.") 1)))
+                 (fn [_] (prn "Closing context c1."))))
 
-(def c2 (Context (fn [] (prn "Opening context c2.") 2)
+(def c2 (Context (fn [] (prn "Opening context c2.") 2.5)
                  (fn [_] (prn "Closing context c2."))))
 
 (def c3 (Context (fn [] (prn "Opening lazy context c3.") 3)
                  nil))
 
-(def c4 (Context (fn [] (prn "Opening lazy context c4.") 4)
+(def c4 (Context (fn [] (prn "Opening lazy context c4.") 4.5)
                  nil))
 
 (def c5 (Context (fn [] (prn "Opening lazy context c5.") 3)
                  nil))
 
-(def c6 (Context (fn [] (prn "Opening lazy context c6.") 4)
+(def c6 (Context (fn [] (prn "Opening lazy context c6.") 4.5)
                  nil))
 
 (def t1 (Test '[a b] [c1 c2]
@@ -184,14 +197,18 @@
               '[(do (prn "Evaluating test t3-a.") (integer? a))
                 (do (prn "Evaluating test t3-b.") (integer? b))]))
 
-(def t4 (Test '[a b] [c5 c6]
+(def t4 (Test '[a b] [c3 c4]
               '[(do (prn "Evaluating test t4-a.") (integer? a))
                 (do (prn "Evaluating test t4-b.") (integer? b))]))
 
+(def t5 (Test '[a b] [c5 c6]
+              '[(do (prn "Evaluating test t5-a.") (integer? a))
+                (do (prn "Evaluating test t5-b.") (integer? b))]))
+
 (def s1 (Suite [] [t1 t2]))
 
-;; Including context c2 in the Suite means it's only executed once for
-;; all tests in the suite.
+;; Attaching a Context to a Suite ensures that Context is evaluated
+;; only once for the entire Suite.
 (def s2 (Suite [c1] [t1 t2]))
 
 (def s3 (Suite [c1 c2] [t1 t2]))
@@ -202,7 +219,12 @@
 
 (def s5 (Suite [c3 c4] [t3 t4]))
 
+(def s6 (Suite [c3 c4] [t4 t5]))
+
 ;; Suites may be nested.
+(def s7 (Suite [] [s3 s5]))
+
+(def s8 (Suite [c1 c2 c3 c4] [s3 s5]))
 
 ;; user=> (run s2)
 ;; ...
