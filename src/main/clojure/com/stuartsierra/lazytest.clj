3e5191ff319fa2e538de4e79c67f1e00529d83e1
Stuart Sierra <mail@stuartsierra.com>
2010-02-23 12:18:15 -0500

    Add ns declaration in lazytest.clj

--- src/main/clojure/com/stuartsierra/lazytest.clj
+++ src/main/clojure/com/stuartsierra/lazytest.clj
@@ -6,11 +6,16 @@
 ;; distribution terms for this software are covered by the Eclipse
 ;; Public License 1.0, which can be found at
 ;; http://opensource.org/licenses/eclipse-1.0.php
+;; and in the file LICENSE.html at the root if this distribution.
 ;;
 ;; By using this software in any fashion, you are agreeing to be bound
 ;; by the terms of this license.  You must not remove this notice, or
 ;; any other, from this software.
 
+
+(ns #^{:doc "Lazy Testing Framework"
+       :author "Stuart Sierra"}
+  com.stuartsierra.lazytest)
 
 ;;; PROTOCOLS
 
