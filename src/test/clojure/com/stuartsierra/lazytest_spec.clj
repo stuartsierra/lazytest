4e5e0e4d9a4d8405f31ba66baec3840f8585e531
Stuart Sierra <mail@stuartsierra.com>
2010-04-12 12:16:27 -0400

    Add specs for assertion function evaluations

--- src/test/clojure/com/stuartsierra/lazytest_spec.clj
+++ src/test/clojure/com/stuartsierra/lazytest_spec.clj
@@ -14,74 +14,86 @@
   (given [x dummy-context-1]
          (is (= x 1) "hello" (= x 2))))
 
-(spec is-spec "The 'is' macro"
-      (given [it is-without-givens]
-             (spec "without any givens"
-                   (is "should create a spec"
-                       (spec? it)
+(describe
+ *ns* 
+ (spec is-spec "The 'is' macro"
+       (given [it is-without-givens]
+              (spec "without any givens"
+                    (is "should create a spec"
+                        (spec? it)
               
-                       "should create an invokable fn"
-                       (instance? clojure.lang.IFn it)
+                        "should create an invokable fn"
+                        (instance? clojure.lang.IFn it)
 
-                       "should attach :doc string metadata to assertions"
-                       (= "hello" (:doc (meta (second (:children it)))))
+                        "should attach :doc string metadata to assertions"
+                        (= "hello" (:doc (meta (second (:children it)))))
 
-                       "should attach :form metadata to assertions"
-                       (= '(= 1 1) (:form (meta (first (:children it))))))
+                        "should attach :form metadata to assertions"
+                        (= '(= 1 1) (:form (meta (first (:children it))))))
 
-                   (spec "should have children"
-                         (is (seq (:children it))
+                    (spec "should have children"
+                          (is (seq (:children it))
 
-                             "that are specs"
-                             (every? spec? (:children it))
+                              "that are specs"
+                              (every? spec? (:children it))
 
-                             "that are invokable IFns"
-                             (every? #(instance? clojure.lang.IFn %) (:children it))
+                              "that are invokable IFns"
+                              (every? #(instance? clojure.lang.IFn %) (:children it))
 
-                             "as many as there are expressions"
-                             (= 2 (count (:children it)))))))
+                              "as many as there are expressions"
+                              (= 2 (count (:children it)))))))
 
-      (given [it is-with-givens]
-             (spec "with givens"
-                   (is "should create a spec"
-                       (spec? it)
+       (given [it is-with-givens]
+              (spec "with givens"
+                    (is "should create a spec"
+                        (spec? it)
               
-                       "should create an invokable IFns"
-                       (instance? clojure.lang.IFn it)
+                        "should create an invokable IFns"
+                        (instance? clojure.lang.IFn it)
 
-                       "should attach :doc string metadata to assertions"
-                       (= "hello" (:doc (meta (second (:children it)))))
+                        "should attach :doc string metadata to assertions"
+                        (= "hello" (:doc (meta (second (:children it)))))
 
-                       "should attach :form metadata to assertions"
-                       (= '(= x 1) (:form (meta (first (:children it))))))
+                        "should attach :form metadata to assertions"
+                        (= '(= x 1) (:form (meta (first (:children it))))))
 
-                   (spec "should have children"
-                         (is (seq (:children it))
+                    (spec "should have children"
+                          (is (seq (:children it))
 
-                             "that are specs"
-                             (every? spec? (:children it))
+                              "that are specs"
+                              (every? spec? (:children it))
 
-                             "that are invokable IFns"
-                             (every? #(instance? clojure.lang.IFn %) (:children it))
+                              "that are invokable IFns"
+                              (every? #(instance? clojure.lang.IFn %) (:children it))
 
-                             "as many as there are expressions"
-                             (= 2 (count (:children it)))
+                              "as many as there are expressions"
+                              (= 2 (count (:children it)))
 
-                             (spec "with contexts"
-                                   (is (seq (:contexts (first (:children it))))
-                                       "that are Context objects"
-                                       (every? context? (:contexts (first (:children it)))))))))))
+                              (spec "with contexts"
+                                    (is (seq (:contexts (first (:children it))))
+                                        "that are Context objects"
+                                        (every? context? (:contexts (first (:children it)))))))))))
 
-(comment
-  (spec assertions-spec "Assertions, when invoked"
-        (given [it is-without-givens]
-               (spec "should return an object"
-                     (is (not (nil? ((:first (:children it)))))
-                         "that supports success?"
-                         (ok? (success? ((:first (:children it)))))
-                         "that supports pending?"
-                         (ok? (pending? ((:first (:children it)))))
-                         "that supports error?"
-                         (ok? (error? ((:first (:children it)))))
-                         "that supports container?"
-                         (ok? (container? ((:first (:children it))))))))))
+ (spec assertions-spec "Assertions, when invoked"
+       (given [it is-without-givens]
+              (spec "should return an object"
+                    (is (not (nil? ((first (:children it)))))
+                        "that supports success?"
+                        (ok? (success? ((first (:children it)))))
+                        "that supports pending?"
+                        (ok? (pending? ((first (:children it)))))
+                        "that supports error?"
+                        (ok? (error? ((first (:children it)))))
+                        "that supports container?"
+                        (ok? (container? ((first (:children it)))))))))
+
+ (spec passing-assertion "A passing assertion"
+       (given [it is-without-givens]
+              (is "should return true for success?"
+                  (true? (success? ((first (:children it)))))
+                  "should return false for error?"
+                  (false? (error? ((first (:children it)))))
+                  "should return false for pending?"
+                  (false? (pending? ((first (:children it)))))
+                  "should return false for container?"
+                  (false? (pending? ((first (:children it)))))))))
