0845795b4b56b632be09ae78eb45aac6a42d193c
Stuart Sierra <mail@stuartsierra.com>
2010-04-26 11:42:44 -0400

    Load assertions file in runtests.clj

--- src/scripts/runtests.clj
+++ src/scripts/runtests.clj
@@ -1,4 +1,6 @@
 (use '[com.stuartsierra.lazytest :only (run-spec)]
      '[com.stuartsierra.lazytest.report :only (report-and-exit)])
 
+(require 'lazytest-asserts)
+
 (report-and-exit (run-spec "src/test"))
