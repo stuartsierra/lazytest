25c391dc44d7aae2480c7776161150d52f52db22
Stuart Sierra <mail@stuartsierra.com>
2010-05-06 10:10:08 -0400

    Move result types/protocols to lazytest.results

---
+++ src/main/clojure/com/stuartsierra/lazytest/results.clj
@@ -0,0 +1,35 @@
+(ns com.stuartsierra.lazytest.results)
+
+(defprotocol TestResult
+  (success? [r] "True if this result and all its children passed.")
+  (pending? [r] "True if this is the result of an empty test.")
+  (error? [r] "True if this is the result of a thrown exception.")
+  (container? [r] "True if this is a container for other results."))
+
+(defrecord TestResultContainer [source children]
+  TestResult
+    (success? [this] (every? success? children))
+    (pending? [this] (if (seq children) false true))
+    (error? [this] false)
+    (container? [this] true))
+
+(defrecord TestPassed [source states]
+  TestResult
+    (success? [this] true)
+    (pending? [this] false)
+    (error? [this] false)
+    (container? [this] false))
+
+(defrecord TestFailed [source states]
+  TestResult
+    (success? [this] false)
+    (pending? [this] false)
+    (error? [this] false)
+    (container? [this] false))
+
+(defrecord TestThrown [source states throwable]
+  TestResult
+    (success? [this] false)
+    (pending? [this] false)
+    (error? [this] true)
+    (container? [this] false))
