da5f93ffb51a44608d70f8903d06e8179f16319d
Stuart Sierra <mail@stuartsierra.com>
2010-05-06 14:42:57 -0400

    Delete old assertion, script, test, and example files

--- src/examples/foo.clj
+++
@@ -1,8 +0,0 @@
-(ns foo
-  (:use com.stuartsierra.lazytest))
-
-(defn foo [] true)
-
-(describe *ns* "The foo namespace"
-          (spec "has a function foo"
-                (is (foo))))
--- src/scripts/runtests.clj
+++
@@ -1,6 +0,0 @@
-(use '[com.stuartsierra.lazytest :only (run-spec)]
-     '[com.stuartsierra.lazytest.report :only (report-and-exit)])
-
-(require 'lazytest-asserts)
-
-(report-and-exit (run-spec "src/test"))
