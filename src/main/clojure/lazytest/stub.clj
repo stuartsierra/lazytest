92d6f498629d1138e109026858c4ab0330b033de
Stuart Sierra <mail@stuartsierra.com>
2010-07-09 23:17:26 -0400

    Rename Mock to Stub
    See http://martinfowler.com/articles/mocksArentStubs.html

--- src/main/clojure/lazytest/stub.clj
+++ src/main/clojure/lazytest/stub.clj
@@ -1,20 +1,20 @@
-(ns lazytest.mock
-  "A Mock is a special kind of Fixture that rebinds a Var in the
+(ns lazytest.stub
+  "A Stub is a special kind of Fixture that rebinds a Var in the
   current dynamic environment."
   (:use [lazytest.fixture :only (Fixture)]))
 
-(deftype ThreadLocalMock [v value]
+(deftype ThreadLocalStub [v value]
   Fixture
   (setup [this] (push-thread-bindings {v value}))
   (teardown [this] (pop-thread-bindings)))
 
-(deftype GlobalMock [v new-value]
+(deftype GlobalStub [v new-value]
   Fixture
   (setup [this]
     (alter-meta! v (fn [m]
 		     (when (contains? m this)
 		       (throw (IllegalStateException.
-			       (str "This global mock is already active for " v))))
+			       (str "This global stub is already active for " v))))
 		     (let [old-value (var-get v)]
 		       (alter-var-root v (constantly new-value))
 		       (assoc m this old-value)))))
@@ -23,21 +23,21 @@
     (alter-meta! v (fn [m]
 		     (when (not (contains? m this))
 		       (throw (IllegalStateException.
-			       (str "This global mock is not active for " v))))
+			       (str "This global stub is not active for " v))))
 		     (alter-var-root v (constantly (get m this)))
 		     (dissoc m this)))))
 
-(defn mock
+(defn stub
   "Returns a Fixture that creates a thread-local binding of Var v to
   new-value."
   [v new-value]
   {:pre [(var? v)]}
-  (ThreadLocalMock. v new-value))
+  (ThreadLocalStub. v new-value))
 
-(defn global-mock
+(defn global-stub
   "Returns a Fixture that modifies the root binding if Var v. The Var
-  must have a root binding before 'setup' is called. Use 'mock'
-  instead unless you need to mock the Var on all threads."
+  must have a root binding before 'setup' is called. Use 'stub'
+  instead unless you need to stub the Var on all threads."
   [v new-value]
   {:pre [(var? v)]}
-  (GlobalMock. v new-value))
+  (GlobalStub. v new-value))
--- src/test/clojure/lazytest/stub_asserts.clj
+++ src/test/clojure/lazytest/stub_asserts.clj
@@ -1,39 +1,39 @@
-(ns lazytest.mock-asserts
+(ns lazytest.stub-asserts
   (:use [lazytest.fixture :only (setup teardown)]
-	lazytest.mock))
+	lazytest.stub))
 
 (defn foo [] 0)
 
-(def mock-1 (mock #'foo (constantly 1)))
-(def mock-2 (mock #'foo (constantly 2)))
+(def stub-1 (stub #'foo (constantly 1)))
+(def stub-2 (stub #'foo (constantly 2)))
 
 (let [] ;; need lexical context for push/pop bindings
   (assert (= 0 (foo)))
-  (setup mock-1)
+  (setup stub-1)
   (try
     (assert (= 1 (foo)))
-    (setup mock-2)
+    (setup stub-2)
     (try
       (assert (= 2 (foo)))
-      (finally (teardown mock-2)))
+      (finally (teardown stub-2)))
     (assert (= 1 (foo)))
-    (finally (teardown mock-1)))
+    (finally (teardown stub-1)))
   (assert (= 0 (foo))))
 
 
-(def gmock-1 (global-mock #'foo (constantly 1)))
-(def gmock-2 (global-mock #'foo (constantly 2)))
+(def gstub-1 (global-stub #'foo (constantly 1)))
+(def gstub-2 (global-stub #'foo (constantly 2)))
 
 (let [] ;; need lexical context for push/pop bindings
   (assert (= 0 (foo)))
-  (setup gmock-1)
+  (setup gstub-1)
   (try
     (assert (= 1 (foo)))
-    (setup gmock-2)
+    (setup gstub-2)
     (try
       (assert (= 2 (foo)))
-      (finally (teardown gmock-2)))
+      (finally (teardown gstub-2)))
     (assert (= 1 (foo)))
-    (finally (teardown gmock-1)))
+    (finally (teardown gstub-1)))
   (assert (= 0 (foo))))
 
