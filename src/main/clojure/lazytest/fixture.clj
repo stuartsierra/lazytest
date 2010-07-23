9f8fd46912e33c28c1dabf984ced3fb03fcaffb0
Stuart Sierra <mail@stuartsierra.com>
2010-07-23 10:35:38 -0400

    Clean up fixture constructor documentation

--- src/main/clojure/lazytest/fixture.clj
+++ src/main/clojure/lazytest/fixture.clj
@@ -9,25 +9,28 @@
 (defn fixture? [x]
   (extends? Fixture (type x)))
 
-(deftype ConstantFixture [value]
-  Fixture
-  (setup [this] value)
-  (teardown [this] nil))
-
-(defn constant-fixture
-  "Returns a fixture whose setup method always returns value and
-  whose teardown method does nothing."
-  [value]
-  (ConstantFixture. value))
-
 (deftype FunctionFixture [f]
   Fixture
   (setup [this] (f))
   (teardown [this] nil))
 
 (defn function-fixture
-  "Returns a fixture whose setup method calls and returns the value of
-  f and whose teardown method does nothing."
+  "Returns a fixture whose setup method returns the result of calling
+  f. The fixture's teardown method does nothing."
   [f]
   {:pre [(fn? f)]}
   (FunctionFixture. f))
+
+(deftype MemoizedFixture [f cache]
+  Fixture
+  (setup [this]
+	 (swap! cache (fn [value]
+			(if (= ::unset value) (f) value))))
+  (teardown [this] nil))
+
+(defn memoized-fixture
+  "Returns a fixture whose setup method calls, returns, and caches the
+  value of f. The fixture's teardown method does nothing."
+  [f]
+  {:pre [(fn? f)]}
+  (MemoizedFixture. f (atom ::unset)))
