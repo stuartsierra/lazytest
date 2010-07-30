1eba278ec0c6f77422974f338c76aec659e2117f
Stuart Sierra <mail@stuartsierra.com>
2010-07-30 18:31:58 -0400

    Support new failure types in console reporter

--- src/main/clojure/lazytest/report/console.clj
+++ src/main/clojure/lazytest/report/console.clj
@@ -5,5 +5,55 @@
 	[clojure.stacktrace :only (print-cause-trace)]
 	[clojure.java.io :only (file)]))
 
+(defn indent [level]
+  (dotimes [i level] (print "    ")))
+
+(defprotocol ConsoleTestReport
+  (p [r l] "Print result r to the console, indented to level l"))
+
+(defn docs [r]
+  (:doc (meta (:source r))))
+
+(defn location [r]
+  (let [m (meta (:source r))]
+    (str (:file m) ":" (:line m))))
+
+(extend-protocol ConsoleTestReport
+  lazytest.result.TestResultGroup
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r) (if (success? r) :green :red)))
+     (doseq [c (:children r)]
+       (p c (inc l))))
+
+  lazytest.result.Pass
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r) :green)))
+
+  lazytest.result.Fail
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r) :red))
+     (println "at" (location r))
+     (pprint (:reason r)))
+
+  lazytest.result.Thrown
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r) :red))
+     (println "at" (location r))
+     (print-cause-trace (:throwable r)))
+
+  lazytest.result.Skip
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r) :blue)))
+
+  lazytest.result.Pending
+  (p [r l]
+     (indent l)
+     (println (colorize (docs r)) :blue)))
+
 (defn report [results]
-  (pprint results))
+  (doseq [r results] (p r 0)))
