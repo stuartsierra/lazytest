d0424cb00510c081665f59ec97b9eda4aec0cc1a
Stuart Sierra <mail@stuartsierra.com>
2010-08-14 10:54:09 -0400

    Add metadata and docstring to 'expect'

--- src/main/clojure/lazytest/expect.clj
+++ src/main/clojure/lazytest/expect.clj
@@ -64,18 +64,35 @@
 (defmacro expect
   "Evaluates expression.  If it returns logical true, returns that
   result.  If the expression returns logical false, throws
-  lazytest.ExpectationFailed with an attached object describing the
-  reason for failure."
-  [expr]
-  (if (function-call? expr)
-    `(let [f# ~(first expr)
-	   args# (list ~@(rest expr))
-	   result# (apply f# args#)]
-       (or result#
-	   (throw (ExpectationFailed. {:form '~expr
-				       :evaluated (list* f# args#)
-				       :result result#}))))
-    `(let [result# ~expr]
-       (or result#
-	   (throw (ExpectationFailed. {:form '~expr
-				       :result result#}))))))
+  lazytest.ExpectationFailed with an attached map describing the
+  reason for failure.  Metadata on expr and on the 'expect' form
+  itself will be merged into the failure map."
+  ([expr] `(expect nil ~expr))
+  ([docstring expr]
+     {:pre [(or (string? docstring) (nil? docstring))]}
+     (if (function-call? expr)
+       ;; Normal function call
+       `(let [f# ~(first expr)
+	      args# (list ~@(rest expr))
+	      result# (apply f# args#)]
+	  (or result#
+	      (throw (ExpectationFailed.
+		      (merge '~(meta &form)
+			     '~(meta expr)
+			     {:form '~expr
+			      :evaluated (list* f# args#)
+			      :result result#
+			      :file ~*file*
+			      :ns *ns*}
+			     ~(when docstring {:doc docstring}))))))
+       ;; Unknown type of expression
+       `(let [result# ~expr]
+	  (or result#
+	      (throw (ExpectationFailed.
+		      (merge '~(meta &form)
+			     '~(meta expr)
+			     {:form '~expr
+			      :result result#
+			      :file ~*file*
+			      :ns *ns*}
+			     ~(when docstring {:doc docstring})))))))))
