f6b4e082debb1b90f57eedd1b30a7333c21773d3
Stuart Sierra <mail@stuartsierra.com>
2010-08-21 21:32:31 -0400

    Add 'all' macro in 'describe' namespace

---
+++ src/test/clojure/examples/describe1.clj
@@ -0,0 +1,12 @@
+(ns examples.describe1
+  (:use lazytest.describe))
+
+(describe +
+  (it "computes the sum of 3 and 4"
+    (= 7 (+ 3 4))))
+
+(describe + "given any 2 integers"
+  (all 
+    (for [x (range 5), y (range 5)]
+      (it "is commutative"
+	(= (+ x y) (+ y x))))))
