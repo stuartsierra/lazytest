f9bd9261cf27e25d7ce9481be0e343692f3f654c
Stuart Sierra <mail@stuartsierra.com>
2010-08-21 21:38:35 -0400

    Add example using nested describes

--- src/test/clojure/examples/describe1.clj
+++ src/test/clojure/examples/describe1.clj
@@ -9,3 +9,15 @@
   (for [x (range 5), y (range 5)]
     (it "is commutative"
       (= (+ x y) (+ y x)))))
+
+(describe +
+  (describe "with integers"
+    (it "computes sums of small numbers"
+      (= 7 (+ 3 4)))
+    (it "computes sums of large numbers"
+      (= 7000000 (+ 3000000 4000000))))
+  (describe "with floating point"
+    (it "computes sums of small numbers"
+      (= 0.0000007 (+ 0.0000003 0.0000004)))
+    (it "computes sums of large numbers"
+      (= 7000000.0 (+ 3000000.0 4000000.0)))))
