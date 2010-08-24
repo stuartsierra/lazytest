1f183e30107cea4995cbef5a3cacc548131e7375
Stuart Sierra <mail@stuartsierra.com>
2010-08-24 18:19:51 -0400

    Remove unused utility functions in 'describe' ns

--- src/main/clojure/lazytest/describe.clj
+++ src/main/clojure/lazytest/describe.clj
@@ -14,63 +14,16 @@
     [(first args) (next args)]
     [nil args]))
 
-(defn- firsts [coll]
-  (take-nth 2 coll))
-
-(defn- seconds [coll]
-  (take-nth 2 (drop 1 coll)))
-
-(defn- thunk-forms [forms]
-  (map (fn [f] `(fn [] ~f)) forms))
+(defn- merged-metadata [body form docstring extra-attr-map]
+  (merge (when (empty? body) {:pending true})
+	 {:nested-doc docstring, :file *file*, :ns *ns*}
+	 (meta form)
+	 extra-attr-map))
 
 (defn- strcat
   "Concatenate strings, with spaces in between, skipping nil."
   [& args]
   (apply str (interpose \space (remove nil? args))))
-
-(defn- merged-metadata [body form docstring extra-attr-map]
-  (merge (when (empty? body) {:pending true})
-	 {:doc docstring, :file *file*, :ns *ns*}
-	 (meta form)
-	 extra-attr-map))
-
-;;; Local Variable Scope
-
-(let [counter (atom 0)]
-  (defn- local-counter []
-    (swap! counter inc)))
-
-(defmacro bind-local
-  "Establishes a local binding of sym to the state returned by local-scope
-  c in all groups and examples found within body."
-  [form c & body]
-  `(let [~(with-meta (gensym) {::local true
-			       ::order (local-counter)
-			       ::arg form}) ~c]
-     ~@body))
-
-(defmacro wrap-local-scope
-  "Recursively creates local bindings for each pair in bindings vector."
-  [bindings body]
-  {:pre [(or (vector? bindings) (nil? bindings))
-         (even? (count bindings))]}
-  (if (seq bindings)
-    `(bind-local ~(first bindings) ~(second bindings)
-       (wrap-local-scope ~(vec (nnext bindings)) ~body))
-    body))
-
-(defn- find-locals
-  "Returns a vector of locals bound by bind-local in the
-  environment."
-  [env]
-  (vec (sort-by #(::order (meta %))
-                (filter #(::local (meta %)) (keys env)))))
-
-(defn- find-local-binding-forms
-  "Returns a vector of the function argument forms of locals bound by
-  bind-local in the environment."
-  [env]
-  (vec (map #(::arg (meta %)) (find-locals env))))
 
 ;;; Top-level definitions
 
