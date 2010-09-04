0da22d5c6768ace9441b95197e483a566eb9acfc
Stuart Sierra <mail@stuartsierra.com>
2010-09-04 15:53:21 -0400

    Re-introduce Context protocol

--- src/main/clojure/lazytest/context.clj
+++ src/main/clojure/lazytest/context.clj
@@ -2,45 +2,33 @@
   "Contexts provide context and/or state in which to run one or more
   tests.")
 
-(defn context
-  "Creates a context with the given setup and teardown functions."
-  [setup-fn teardown-fn]
-  {:pre [(fn? setup-fn) (fn? teardown-fn)]}
-  (with-meta {:setup setup-fn, :teardown teardown-fn} {:type ::context}))
+(defprotocol Context
+  (setup [this] "Creates and returns state provided by this context.")
+  (teardown [this] "Cleans up state created by this context."))
 
 (defn context?
   "Returns true if x is a context."
   [x]
-  (isa? (type x) ::context))
+  (extends? Context (type x)))
 
-(defn memoized-context
-  "Returns a context whose setup method calls, returns, and caches the
-  value of f. The context's teardown method clears the cache."
-  [f]
-  {:pre [(fn? f)]}
-  (let [cache (atom ::unset)]
-    (context (fn [] (swap! cache
-			   (fn [value]
-			     (if (= ::unset value) (f) value))))
-	     (fn [] (reset! cache ::unset)))))
+(deftype FunctionContext [setup-fn teardown-fn]
+  Context
+  (setup [this] (setup-fn))
+  (teardown [this] (teardown-fn)))
 
-(defn setup 
-  "Invokes the setup function of context c."
-  [c]
-  {:pre [(context? c)]}
-  ((:setup c)))
-
-(defn teardown
-  "Invokes the teardown function of context c."
-  [c]
-  {:pre [(context? c)]}
-  ((:teardown c)))
+(defn fn-context
+  "Creates a context with the given setup and teardown functions."
+  [setup-fn teardown-fn]
+  {:pre [(fn? setup-fn) (fn? teardown-fn)]}
+  (FunctionContext. setup-fn teardown-fn))
 
 (defn add-context
   "Adds context c to metadata of iobj."
-  [iobj c]
-  {:pre [(context? c)]}
-  (vary-meta iobj update-in [::contexts] conj c))
+  ([iobj c]
+     {:pre [(context? c)]}
+     (vary-meta iobj update-in [::contexts] conj c))
+  ([iobj c & more]
+     (reduce add-context (add-context iobj c) more)))
 
 (defn setup-contexts
   "Run setup functions of all contexts in iobj's metadata."
@@ -53,15 +41,3 @@
   [iobj]
   (doseq [c (reverse (::contexts (meta iobj)))]
     (teardown c)))
-
-(defn using-contexts
-  "Returns a no-argument function applying f to the values returned by
-  the setup function of contexts cs.  Ensures that the teardown
-  functions of all contexts are called.
-
-  The returned function has the same metadata as f."
-  [f & cs]
-  {:pre [(fn? f) (every? context? cs)]}
-  (with-meta (fn [] (try (apply f (map setup cs))
-			 (finally (dorun (map teardown cs)))))
-    (meta f)))
--- src/main/clojure/lazytest/context/stub.clj
+++ src/main/clojure/lazytest/context/stub.clj
@@ -1,15 +1,16 @@
 (ns lazytest.context.stub
   "A Stub is a special kind of Context that rebinds a Var in the
   current dynamic environment."
-  (:use [lazytest.context :only (context)]))
+  (:use [lazytest.context :only (Context)]))
 
 (defn stub
   "Returns a Context that creates a thread-local binding of Var v to
   new-value."
   [v new-value]
   {:pre [(var? v)]}
-  (context (fn [] (push-thread-bindings {v new-value}))
-	   (fn [] (pop-thread-bindings))))
+  (reify Context
+	 (setup [this] (push-thread-bindings {v new-value}))
+	 (teardown [this] (pop-thread-bindings))))
 
 (defn global-stub
   "Returns a Context that modifies the root binding if Var v. The Var
@@ -17,21 +18,19 @@
   instead unless you need to stub the Var on all threads."
   [v new-value]
   {:pre [(var? v)]}
-  (let [this-stub (Object.)]
-    (context
-     (fn []
-       (alter-meta! v (fn [m]
-			(when (contains? m this-stub)
-			  (throw (IllegalStateException.
-				  (str "This global stub is already active for " v))))
-			(let [old-value (var-get v)]
-			  (alter-var-root v (constantly new-value))
-			  (assoc m this-stub old-value)))))
-   
-     (fn []
-       (alter-meta! v (fn [m]
-			(when (not (contains? m this-stub))
-			  (throw (IllegalStateException.
-				  (str "This global stub is not active for " v))))
-			(alter-var-root v (constantly (get m this-stub)))
-			(dissoc m this-stub)))))))
+  (reify Context
+	 (setup [this]
+		(alter-meta! v (fn [m]
+				 (when (contains? m this)
+				   (throw (IllegalStateException.
+					   (str "This global stub is already active for " v))))
+				 (let [old-value (var-get v)]
+				   (alter-var-root v (constantly new-value))
+				   (assoc m this old-value)))))
+	 (teardown [this]
+		   (alter-meta! v (fn [m]
+				    (when (not (contains? m this))
+				      (throw (IllegalStateException.
+					      (str "This global stub is not active for " v))))
+				    (alter-var-root v (constantly (get m this)))
+				    (dissoc m this))))))
