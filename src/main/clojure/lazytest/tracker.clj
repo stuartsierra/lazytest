89a0a8cd18918efe5a461b7cde6417a0ac54b70f
Stuart Sierra <mail@stuartsierra.com>
2010-09-18 12:24:51 -0400

    Rename lazytest.loader to lazytest.tracker

--- src/main/clojure/lazytest/tracker.clj
+++ src/main/clojure/lazytest/tracker.clj
@@ -1,47 +1,42 @@
-(ns lazytest.loader
-  "Loading namespaces and managing dependencies."
+(ns lazytest.tracker
+  "Keeping track of which namespaces have changed and need to be reloaded"
   (:use [lazytest.dependency :only (graph depend dependents remove-key)]
 	[lazytest.nsdeps :only (deps-from-ns-decl)]
-	[lazytest.reload :only (reload)]
 	[clojure.contrib.find-namespaces :only (find-clojure-sources-in-dir
 						read-file-ns-decl)]
-	[clojure.set :only (union)]
-	[clojure.java.io :only (file)]))
+	[clojure.set :only (union)]))
 
-(defn find-sources
+(defn- find-sources
   [dirs]
-  {:pre [(coll? dirs)
-	 (every? (fn [d] (instance? java.io.File d)) dirs)]}
+  {:pre [(every? (fn [d] (instance? java.io.File d)) dirs)]}
   (mapcat find-clojure-sources-in-dir dirs))
 
-(defn newer-sources [dirs timestamp]
+(defn- newer-sources [dirs timestamp]
   (filter #(> (.lastModified %) timestamp) (find-sources dirs)))
 
-(defn newer-namespace-decls [dirs timestamp]
+(defn- newer-namespace-decls [dirs timestamp]
   (remove nil? (map read-file-ns-decl (newer-sources dirs timestamp))))
 
-(defn add-to-dep-graph [dep-graph namespace-decls]
+(defn- add-to-dep-graph [dep-graph namespace-decls]
   (reduce (fn [g decl]
 	    (let [nn (second decl)
 		  deps (deps-from-ns-decl decl)]
-	      (if (seq deps)
-		(apply depend g nn deps)
-		g)))
+	      (apply depend g nn deps)))
 	  dep-graph namespace-decls))
 
-(defn remove-from-dep-graph [dep-graph new-decls]
+(defn- remove-from-dep-graph [dep-graph new-decls]
   (apply remove-key dep-graph (map second new-decls)))
 
-(defn update-dependency-graph [dep-graph new-decls]
+(defn- update-dependency-graph [dep-graph new-decls]
   (-> dep-graph
       (remove-from-dep-graph new-decls)
       (add-to-dep-graph new-decls)))
 
-(defn affected-namespaces [changed-namespaces old-dependency-graph]
+(defn- affected-namespaces [changed-namespaces old-dependency-graph]
   (apply union (set changed-namespaces) (map #(dependents old-dependency-graph %)
 					     changed-namespaces)))
 
-(defn reload-observer [dirs initial-timestamp]
+(defn tracker [dirs initial-timestamp]
   "Returns a no-arg function which, when called, returns a set of
   namespaces that need to be reloaded, based on file modification
   timestamps and the graph of namespace dependencies."
