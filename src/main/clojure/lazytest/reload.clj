531264dff1d6635613fb60fae3fd94f0c348350a
Stuart Sierra <mail@stuartsierra.com>
2010-09-19 17:02:21 -0400

    Fix needed :reload-all in reload

--- src/main/clojure/lazytest/reload.clj
+++ src/main/clojure/lazytest/reload.clj
@@ -1,9 +1,9 @@
 (ns lazytest.reload)
 
 (defn reload
-  "Removes all namespaces named by symbols, then reloads them all."
+  "Removes all namespaces named by symbols, then reloads those
+  namespaces and all their dependencies."
   [& symbols]
   {:pre (every? symbol? symbols)}
-  (doseq [sym symbols]
-    (remove-ns sym))
-  (apply require :reload symbols))
+  (doseq [sym symbols] (remove-ns sym))
+  (apply require :reload-all symbols))
