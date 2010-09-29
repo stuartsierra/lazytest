0877530c5b5a4892006773f5616524e36147a602
Stuart Sierra <mail@stuartsierra.com>
2010-09-29 19:23:40 -0400

    Remove libs from *loaded-libs* before reloading
    * More efficient than :reload-all
    * May prevent some load-order errors

--- src/main/clojure/lazytest/reload.clj
+++ src/main/clojure/lazytest/reload.clj
@@ -1,9 +1,17 @@
-(ns lazytest.reload)
+(ns lazytest.reload
+  (:require clojure.set))
+
+(defn remove-from-loaded-libs
+  "Removes symbols from the set in the private Ref clojure.core/*loaded-libs*"
+  [& syms]
+  (dosync
+   (alter @#'clojure.core/*loaded-libs*
+	  clojure.set/difference (set syms))))
 
 (defn reload
-  "Removes all namespaces named by symbols, then reloads those
-  namespaces and all their dependencies."
+  "Removes all namespaces named by symbols, then reloads them."
   [& symbols]
   {:pre (every? symbol? symbols)}
   (doseq [sym symbols] (remove-ns sym))
-  (apply require :reload-all symbols))
+  (apply remove-from-loaded-libs symbols)
+  (apply require symbols))
