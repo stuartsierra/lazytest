b6e3a2e113d536ce07bd182ae575334f32529585
Stuart Sierra <mail@stuartsierra.com>
2010-10-03 12:17:00 -0400

    Touch .clj source files on reload only when needed

--- src/main/clojure/lazytest/reload.clj
+++ src/main/clojure/lazytest/reload.clj
@@ -1,33 +1,55 @@
 (ns lazytest.reload
   (:require [clojure.set :as set]
-	    [clojure.string :as s]))
+	    [clojure.string :as str]))
 
-(defn remove-from-loaded-libs
+(defn- remove-from-loaded-libs
   "Removes symbols from the set in the private Ref clojure.core/*loaded-libs*"
   [& syms]
   (dosync
    (alter @#'clojure.core/*loaded-libs*
 	  set/difference (set syms))))
 
-(defn find-source-file
+(defn- basename
+  "Converts a namespace name symbol to a String file path, without
+  extension."
+  [sym]
+  (-> sym name (str/replace "-" "_") (str/replace "." "/")))
+
+(defn- find-file-on-classpath
+  "Given a path as a string, searches for that file on the classpath,
+  returns a java.io.File. Returns nil if the file cannot be found or
+  is embedded in a JAR."
+  [path]
+  (when-let [u (.. Thread currentThread getContextClassLoader (getResource path))]
+    (when (= "file" (.getProtocol u))
+      (when-let [p (.getPath u)]
+	(java.io.File. p)))))
+
+(defn- find-source-file
   "Attempts to find the source .clj file for the namespace with the
   given name, returns a java.io.File. Returns nil if the file cannot
   be found or is inside a JAR."
   [sym]
-  (let [path (-> sym name (s/replace "-" "_") (s/replace "." "/") (str ".clj"))]
-    (when-let [u (.. Thread currentThread getContextClassLoader (getResource path))]
-      (when (= "file" (.getProtocol u))
-	(when-let [p (.getPath u)]
-	  (java.io.File. p))))))
+  (find-file-on-classpath (str (basename sym) ".clj")))
 
-(defn touch-source-file
-  "Attempts to find the .clj source file for the namespace with the
-  given name, and sets its modification time to the current time.
-  Ensures future loads will use the .clj source file and not the
-  AOT-compiled .class file."
+(defn- has-class-file?
+  "Returns true if the namespace has an AOT-compiled .class file on
+  the classpath."
   [sym]
-  (when-let [f (find-source-file sym)]
-    (.setLastModified f (System/currentTimeMillis))))
+  (boolean (.. Thread currentThread getContextClassLoader
+	       (getResource (str (basename sym) ".class")))))
+
+(defn- ensure-source-file-newer
+  "If the given namespace name has an AOT-compiled .class file,
+  attempts to find the .clj source file and set its modification time
+  to the current time.  Ensures future loads will use the .clj source
+  file and not the AOT-compiled .class file."
+  [sym]
+  (when (has-class-file? sym)
+    (if-let [f (find-source-file sym)]
+      (when-not (.setLastModified f (System/currentTimeMillis))
+	(println "WARNING: failed to update timestamp on source file for namespace" sym))
+      (println "WARNING: failed to find normal source file for namespace" sym))))
 
 (defn reload
   "Removes all namespaces named by symbols, then reloads them."
@@ -35,5 +57,5 @@
   {:pre (every? symbol? symbols)}
   (doseq [sym symbols] (remove-ns sym))
   (apply remove-from-loaded-libs symbols)
-  (doseq [sym symbols] (touch-source-file sym))
+  (doseq [sym symbols] (ensure-source-file-newer sym))
   (apply require symbols))
