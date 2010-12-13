c830796c6fe46334e6d4da517f5c30b032830922
Stuart Sierra <mail@stuartsierra.com>
2010-12-13 07:55:48 -0500

    Switch Clojure to 1.3.0-alpha4

--- modules/sample-leiningen-project/project.clj
+++ modules/sample-leiningen-project/project.clj
@@ -1,6 +1,6 @@
 (defproject sample-leiningen-project "1.0.0-SNAPSHOT"
   :description "This project shows how to use Lazytest in a Leiningen project."
-  :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
 		 [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
 		 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
