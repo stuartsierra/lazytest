ad74daa01a04aba893e84d81a1d7f6e778f67e81
ninjudd <git@justinbalthrop.com>
2010-12-21 17:06:07 -0800

    fix nsdeps to support nested use/require forms like those in penumbra. e.g. src/penumbra/app.clj and src/penumbra/translate/c.clj

--- modules/lazytest/src/main/clojure/lazytest/nsdeps.clj
+++ modules/lazytest/src/main/clojure/lazytest/nsdeps.clj
@@ -2,17 +2,19 @@
   "Parsing namespace declarations for dependency information."
   (:use [clojure.set :only (union)]))
 
+(defn- prepend [prefix sym]
+  (symbol (str (when prefix (str prefix "."))
+               sym)))
+
 (defn- deps-from-libspec [prefix form]
-  (cond (list? form) (apply union (map (fn [f] (deps-from-libspec
-						(symbol (str (when prefix (str prefix "."))
-							     (first form)))
-						f))
-				       (rest form)))
-	(vector? form) (deps-from-libspec prefix (first form))
-	(symbol? form) #{(symbol (str (when prefix (str prefix ".")) form))}
-	(keyword? form) #{}
-	:else (throw (IllegalArgumentException.
-		      (pr-str "Unparsable namespace form:" form)))))
+  (cond (coll? form) (if (or prefix (some keyword? form) (= 1 (count form)))
+                       (deps-from-libspec prefix (first form))
+                       (apply union (map #(deps-from-libspec (prepend prefix (first form)) %)
+                                         (rest form))))
+        (symbol? form) #{(prepend prefix form)}
+        (keyword? form) #{}
+        :else (throw (IllegalArgumentException.
+                      (pr-str "Unparsable namespace form:" form)))))
 
 (defn- deps-from-ns-form [form]
   (when (and (list? form)
--- modules/lazytest/src/test/clojure/lazytest/nsdeps_asserts.clj
+++ modules/lazytest/src/test/clojure/lazytest/nsdeps_asserts.clj
@@ -8,8 +8,8 @@
 (assert (= #{'a.b} (deps-from-ns-decl '(ns x (:use (a [b :only (c)]))))))
 (assert (= #{'a.b 'a.c 'a.d} (deps-from-ns-decl '(ns x (:use (a b c d))))))
 
-;; Nested prefix lists (ugh)
-(assert (= #{'a.b.c 'a.b.d} (deps-from-ns-decl '(ns x (:use (a (b c d)))))))
+;; nested use (penumbra uses libspecs like this)
+(assert (= #{'a.b 'a.c} (deps-from-ns-decl '(ns x (:use (a [b :only (d)] [c :only (e)]))))))
 
 ;; Keyword flags in :use (ugh, but 'lein new' does it by default)
 (assert (= #{'a.b} (deps-from-ns-decl '(ns x (:use [a.b] :reload)))))
